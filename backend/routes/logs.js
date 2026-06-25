d2ea2cab4948a16f63b26918609c6c18add5961d
pugazhmaniT <pugazhspoidy@gmail.com>
2026-06-25 22:29:48 +0530

    Fixed deployment issues and connected frontend with backend

--- backend/routes/logs.js
+++ backend/routes/logs.js
@@ -13,45 +13,54 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + path.extname(file.originalname));
   }
 });
+
 const fileFilter = (req, file, cb) => {
   if (path.extname(file.originalname) !== ".json") {
     return cb(new Error("Only JSON files are allowed"), false);
   }
   cb(null, true);
 };
+
 const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter
+  storage,
+  fileFilter
 });
-// Upload logs-create new record
+
 router.post("/upload", async (req, res) => {
   try {
     const logs = req.body;
-
     await Log.insertMany(logs);
 
     res.status(200).json({
       message: `${logs.length} logs uploaded successfully`
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({
       error: error.message
     });
   }
 });
 
-//read data from Mongo DB
 router.get("/read", async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
     const limit = 20;
     const skip = (page - 1) * limit;
 
-    const { severity, status, region, role, action, search, resourceType } = req.query;
+    const {
+      severity,
+      status,
+      region,
+      role,
+      action,
+      search,
+      resourceType
+    } = req.query;
+
     const { sortBy, order } = req.query;
     let filter = {};
 
-    // Filters
     if (severity) filter.severity = severity;
     if (status) filter.status = status;
     if (region) filter.region = region;
@@ -59,8 +68,6 @@ router.get("/read", async (req, res) => {
     if (action) filter.action = action;
     if (resourceType) filter.resourceType = resourceType;
 
-
-    // Search
     if (search) {
       filter.$or = [
         { actor: { $regex: search, $options: "i" } },
@@ -71,11 +78,12 @@ router.get("/read", async (req, res) => {
 
     const totalRecords = await Log.countDocuments(filter);
     const totalPages = Math.ceil(totalRecords / limit);
-    let sortOption = {};
 
+    let sortOption = {};
     if (sortBy) {
       sortOption[sortBy] = order === "desc" ? -1 : 1;
     }
+
     const logs = await Log.find(filter)
       .sort(sortOption)
       .skip(skip)
@@ -87,24 +95,24 @@ router.get("/read", async (req, res) => {
       currentPage: page,
       logs
     });
-
   } catch (error) {
+    console.error("READ API ERROR:", error);
     res.status(500).json({
       error: error.message
     });
   }
 });
+
 router.post("/upload-file", upload.single("file"), async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({
-        error: " File Format Not supported. Please upload file in json format only"
+        error: "File format not supported. Upload JSON only"
       });
     }
 
     const filePath = req.file.path;
     const fileData = fs.readFileSync(filePath, "utf-8");
-
     const logs = JSON.parse(fileData);
 
     await Log.insertMany(logs);
@@ -113,10 +121,11 @@ router.post("/upload-file", upload.single("file"), async (req, res) => {
       message: `${logs.length} logs uploaded successfully`
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({
       error: error.message
     });
   }
-});63
+});
 
 module.exports = router;
